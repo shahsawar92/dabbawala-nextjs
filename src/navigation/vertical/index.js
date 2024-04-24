5cea28f07dbefdb04f23fd177a5b3abace33367a
tehceen <tahseenullah417@gmail.com>
2024-04-24 11:51:20 +0500

    logo added

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -33,40 +33,40 @@ const navigation = () => {
       path: '/pages/register',
       openInNewTab: true
     },
-    // {
-    //   title: 'Error',
-    //   icon: AlertCircleOutline,
-    //   path: '/pages/error',
-    //   openInNewTab: true
-    // },
-    // {
-    //   sectionTitle: 'User Interface'
-    // },
-    // {
-    //   title: 'Typography',
-    //   icon: FormatLetterCase,
-    //   path: '/typography'
-    // },
-    // {
-    //   title: 'Icons',
-    //   path: '/icons',
-    //   icon: GoogleCirclesExtended
-    // },
-    // {
-    //   title: 'Cards',
-    //   icon: CreditCardOutline,
-    //   path: '/cards'
-    // },
-    // {
-    //   title: 'Tables',
-    //   icon: Table,
-    //   path: '/tables'
-    // },
-    // {
-    //   icon: CubeOutline,
-    //   title: 'Form Layouts',
-    //   path: '/form-layouts'
-    // }
+    {
+      title: 'Error',
+      icon: AlertCircleOutline,
+      path: '/pages/error',
+      openInNewTab: true
+    },
+    {
+      sectionTitle: 'User Interface'
+    },
+    {
+      title: 'Typography',
+      icon: FormatLetterCase,
+      path: '/typography'
+    },
+    {
+      title: 'Icons',
+      path: '/icons',
+      icon: GoogleCirclesExtended
+    },
+    {
+      title: 'Cards',
+      icon: CreditCardOutline,
+      path: '/cards'
+    },
+    {
+      title: 'Tables',
+      icon: Table,
+      path: '/tables'
+    },
+    {
+      icon: CubeOutline,
+      title: 'Form Layouts',
+      path: '/form-layouts'
+    }
   ]
 }
 
